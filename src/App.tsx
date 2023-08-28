dbec454392d71625fe87fa6580c3d32011adc95f
artkas03 <zhonayer2017@gmail.com>
2023-08-29 01:57:23 +0300

    added variables and image import example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { HomePage } from './components/HomePage';
+// import logo from './assets/img/Logo.png'; example of importing image 1.1
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+      {/* <img src={logo} alt="text" /> example of importing image 1.2 */}
     </div>
   );
 }
