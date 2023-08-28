c5f5a0e69081be3934a5d8163eecf9a3a1cd0d05
artkas03 <zhonayer2017@gmail.com>
2023-08-29 02:46:51 +0300

    implemented notfoundpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { HomePage } from './components/HomePage';
-// import logo from './assets/img/Logo.png'; example of importing image 1.1
+import { NotFoundPage } from './components/NotFoundPage';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
-      {/* <img src={logo} alt="text" /> example of importing image 1.2 */}
+      <NotFoundPage />
     </div>
   );
 }
---
+++ src/components/NotFoundPage/NotFountPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './NotFountPage.scss';
+
+export const NotFoundPage: React.FC = () => {
+  return (
+    <div className="notfoundpage">
+      <h1>Page is not found</h1>
+    </div>
+  );
+};
