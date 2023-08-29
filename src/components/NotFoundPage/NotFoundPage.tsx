bed99ec77e58ef568ab1cc5789a91c151410616c
artkas03 <zhonayer2017@gmail.com>
2023-08-29 18:04:38 +0300

    test

--- src/components/NotFoundPage/NotFoundPage.tsx
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -4,7 +4,7 @@ import './NotFoundPage.scss';
 export const NotFoundPage: React.FC = () => {
   return (
     <div className="not-found-page">
-      <h1>Page is not found</h1>
+      <h1>Page is not foun</h1>
     </div>
   );
 };
