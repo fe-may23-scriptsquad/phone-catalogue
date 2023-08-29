4aa0830a260b42596cbacd3bdfda0566e77e37e3
artkas03 <zhonayer2017@gmail.com>
2023-08-29 21:29:07 +0300

    merged with card feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
 import React from 'react';
-import { CardItem } from './components/CardItem';
-import { Phone } from './types/Phone';
-
-const phone: Phone = {
-  id: '1',
-  category: 'phones',
-  phoneId: 'apple-iphone-7-32gb-black',
-  itemId: 'apple-iphone-7-32gb-black',
-  name: 'Apple iPhone 7 32GB Black',
-  fullPrice: 400,
-  price: 375,
-  screen: '4.7\' IPS',
-  capacity: '32GB',
-  color: 'black',
-  ram: '2GB',
-  year: 2016,
-  image: 'img/phones/apple-iphone-7/black/00.jpg',
-};
+import { HomePage } from './components/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <CardItem phone={phone} />
+      <HomePage />
     </div>
   );
 }
