8bf754bbb924b4013915f899e3f87dbbeef7f886
outofs <rtranscendental@gmail.com>
2023-08-29 16:54:37 +0300

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,8 @@
 import React from 'react';
 import './App.scss';
-import { NotFoundPage } from './components/NotFoundPage';
-import { CardItem } from './components/CardItem';
-
-const testData = {
-  id: '1',
-  category: 'phones',
-  phoneId: 'apple - iphone - 7 - 32gb- black',
-  itemId: 'apple - iphone - 7 - 32gb - black',
-  name: 'Apple iPhone 7 32GB Black',
-  fullPrice: 400,
-  price: 375,
-  screen: "4.7' IPS",
-  capacity: '32GB',
-  color: 'black',
-  ram: '2GB',
-  year: 2016,
-  image: 'img/phones/apple-iphone-7/black/00.jpg',
-};
 
 function App() {
-  return (
-    <div className="App">
-      <NotFoundPage />
-      <CardItem phone={testData} />
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
--- src/components/CardItem/CardItem.tsx
+++ src/components/CardItem/CardItem.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import './card-item.scss';
 import { Phone } from '../../types/Phone';
 import { Button } from '../Button';
+import like from '../../assets/icons/Favourites.svg';
 
 type Props = {
   phone: Phone;
 };
 
 // eslint-disable-next-line max-len
-const testImg = 'https://www.91-img.com/pictures/143993-v4-apple-iphone-14-mobile-phone-large-4.jpg';
+const one = 'https://www.91-img.com/pictures/';
+const two = '143993-v4-apple-iphone-14-mobile-phone-large-4.jpg';
+const testImg = `${one}${two}`;
 
 export const CardItem: React.FC<Props> = ({ phone }) => {
   return (
@@ -47,9 +50,9 @@ export const CardItem: React.FC<Props> = ({ phone }) => {
       <div className="card__btns">
         <Button text="Add to cart" handler={() => {}} />
 
-        <button type="button" className="button-like">
-          Like
-        </button>
+        <a href="/#" className="button-like">
+          <img className="icon" src={like} alt="like" />
+        </a>
       </div>
     </div>
   );
