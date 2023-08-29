74e07bd79c2e5b288207e79d6e7c9645a4b08d6e
artkas03 <zhonayer2017@gmail.com>
2023-08-29 22:01:12 +0300

    implemented not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { HomePage } from './components/HomePage';
+import { NotFoundPage } from './components/NotFoundPage';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <NotFoundPage />
     </div>
   );
 }
