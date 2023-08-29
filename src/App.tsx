dfe4038ca9e858f3a4a56dcc37ca65497252e029
Artem Kasianov <72705169+artkas03@users.noreply.github.com>
2023-08-29 15:56:29 +0300

    Revert "Header without burger menu"

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Header } from './components/Header';
+import { NotFoundPage } from './components/NotFoundPage';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <NotFoundPage />
     </div>
   );
 }
