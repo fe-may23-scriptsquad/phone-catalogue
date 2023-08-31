79ff0f32c5bf92fc78c77f5b39c3829c57a5f4be
artkas03 <zhonayer2017@gmail.com>
2023-08-31 12:31:13 +0300

    implemented base banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
 import React from 'react';
-import { Header } from './components/Header';
-import { CardItem } from './components/CardItem';
-
-const phone = {
-  id: '1',
-  category: 'phones',
-  phoneId: 'apple-iphone-7-32gb-black',
-  itemId: 'apple-iphone-7-32gb-black',
-  name: 'Apple iPhone 7 32GB Black',
-  fullPrice: 400,
-  price: 375,
-  screen: "4.7' IPS",
-  capacity: '32GB',
-  color: 'black',
-  ram: '2GB',
-  year: 2016,
-  image: 'img/phones/apple-iphone-7/black/00.jpg',
-};
+import { Banner } from './pages/HomePage/сomponents/Banner';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <CardItem phone={phone} />
+      <Banner />
     </div>
   );
 }
