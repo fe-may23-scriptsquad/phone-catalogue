ffc6d5252d46eed0ccd16e2c1975cec0d7f5b343
Daniil Postoiuk <daniil.postoyuk@gmail.com>
2023-09-01 11:17:18 +0300

    add all routes without navLinks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 
-import { HomePage } from './components/HomePage';
 import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
 
       <div className="container">
         <Outlet />
+        <Footer />
       </div>
     </div>
   );
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,14 +1,23 @@
-import { Route, HashRouter as Router } from 'react-router-dom';
+import { Route, Routes, HashRouter as Router } from 'react-router-dom';
 import App from './App';
-import { HomePage } from './components/HomePage';
+import { HomePage } from './pages/HomePage';
+import { NotFoundPage } from './pages/NotFoundPage';
 
 export const Root = () => (
   <Router>
-    <Route path="/" element={<App />}>
-      <Route index element={<HomePage />} />
-      <Route path="phones" />
-    </Route>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path="phones">
+          <Route path=":phoneId?" />
+        </Route>
+        <Route path="tablets" />
+        <Route path="accessories" />
+        <Route path="favourites" />
+        <Route path="cart" />
+      </Route>
 
-    <Route path="*" element={<p>Page not found</p>} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   </Router>
 );
