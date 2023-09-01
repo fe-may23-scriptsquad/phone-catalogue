bc74fe6a84cb7cf9b85737d7eb3d813b98b21414
outofs <rtranscendental@gmail.com>
2023-09-01 16:56:05 +0300

    Adding test Cart logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
 import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { CartPage } from './pages/CartPage';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <div className="container">
+        <CartPage />
+      </div>
       <Footer />
     </div>
   );
---
+++ src/types/Order.ts
@@ -0,0 +1,6 @@
+import { Phone } from './Phone';
+
+export interface Order {
+  product: Phone;
+  quantity: number;
+}
