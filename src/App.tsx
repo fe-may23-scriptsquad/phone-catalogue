dcc4f5784034618c017706b5dc55f50d8309f0ad
artkas03 <zhonayer2017@gmail.com>
2023-09-03 20:33:14 +0300

    moved footer out of container

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Outlet } from 'react-router-dom';
-
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 
@@ -7,11 +6,10 @@ function App() {
   return (
     <div className="App">
       <Header />
-
       <div className="container">
         <Outlet />
-        <Footer />
       </div>
+      <Footer />
     </div>
   );
 }
