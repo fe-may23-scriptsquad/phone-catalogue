64eb10110965f40b3f260cf9a932570172b13435
artkas03 <zhonayer2017@gmail.com>
2023-09-04 03:07:19 +0300

    started making homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,52 @@
 import React from 'react';
 import { SwiperPhones } from '../../components/SwiperPhones';
+import { Category } from './components/Category';
+
+const tempData = [{
+  linkTo: '/phones',
+  title: 'Mobile phones',
+}, {
+  linkTo: '/tablets',
+  title: 'Tablets',
+}, {
+  linkTo: '/accessories',
+  title: 'Accessories',
+}];
 
 export const HomePage: React.FC = () => {
-  return <SwiperPhones />;
+  return (
+    <div className="homepage">
+      <div className="homepage__header">
+        <h1 className="homepage__header-title">
+          Welcome to Nice Gadgets store!
+        </h1>
+
+        <span className="homepage__header-futurebanner">
+          there will be the banner
+        </span>
+      </div>
+
+      <div className="homepage__body">
+        <SwiperPhones />
+
+        <div className="homepage__categories">
+          <h2 className="homepage__blocktitle">
+            Shop by category
+          </h2>
+
+          <div className="homepage__categories-links">
+            {tempData.map((category) => (
+              <Category
+                linkTo={category.linkTo}
+                title={category.title}
+                key={category.title}
+              />
+            ))}
+          </div>
+        </div>
+
+        <SwiperPhones />
+      </div>
+    </div>
+  );
 };
---
+++ src/pages/HomePage/components/Category/Category.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import image from '../../../../assets/img/categories/category-tablets.png';
+
+type Props = {
+  linkTo: string,
+  title: string,
+};
+
+export const Category: React.FC<Props> = ({
+  linkTo,
+  title,
+}) => {
+  return (
+    <div className="category">
+      <Link to={linkTo} className="category__image">
+        <img
+          src={image}
+          alt={`link to ${linkTo.slice(1)}`}
+        />
+      </Link>
+      <Link to={linkTo} className="category__title">
+        {title}
+      </Link>
+      <p className="category__products-amount">
+        0 models
+      </p>
+    </div>
+  );
+};
