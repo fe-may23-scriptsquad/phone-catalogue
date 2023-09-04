e446124c882a633b3a0c70dbe74e5806ddca02d0
NK <nadiaaaa1256@gmail.com>
2023-09-04 23:54:14 +0300

    create catalog component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 // import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
-import { Banner } from './pages/HomePage/сomponents/Banner';
+import { Catalog } from './pages/Catalog';
+import phonesFromServer from './api/phones.json';
+
+const productName = 'Mobile phones';
+const pathName = ['Phones'];
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="container">
-        <Banner />
+        <Catalog
+          productName={productName}
+          phones={phonesFromServer}
+          pathName={pathName}
+        />
       </div>
       <Footer />
     </div>
