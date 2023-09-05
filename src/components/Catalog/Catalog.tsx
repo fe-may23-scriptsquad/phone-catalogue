7589993ad12cc68d5dc930685a5740f28c20a319
artkas03 <zhonayer2017@gmail.com>
2023-09-05 12:54:55 +0300

    merged with catalag

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -1,15 +1,17 @@
 /* eslint-disable max-len */
 import { useState } from 'react';
-import { Pagination } from '../../components/Pagination';
-import { CardItem } from '../../components/CardItem';
+import { Pagination } from '../Pagination';
+import { CardItem } from '../CardItem';
 import { Phone } from '../../types/Phone';
 import home from '../../assets/icons/Home.svg';
-import { Dropdown } from '../../components/Dropdown';
+import { Dropdown } from '../Dropdown';
+
+import phonesFromServer from '../../api/phones.json';
 
 type CatalogProps = {
-  productName: string;
-  phones: Phone[];
-  pathName: string[];
+  productName?: string;
+  phones?: Phone[];
+  pathName?: string[];
 };
 
 const sortOptions = [
@@ -25,7 +27,11 @@ const itemsOnPageOptions = [
   'All',
 ];
 
-export const Catalog = ({ productName, phones, pathName }: CatalogProps) => {
+export const Catalog = ({
+  productName = 'Mobile phones',
+  phones = phonesFromServer,
+  pathName = ['Phones'],
+}: CatalogProps) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [sortOption, setSortOption] = useState('Newest');
   const [phonesPerPage, setPhonesPerPage] = useState('8');
@@ -85,7 +91,7 @@ export const Catalog = ({ productName, phones, pathName }: CatalogProps) => {
                 const crumbPath = `/${pathName.slice(0, index + 1).join('/')}`;
 
                 return (
-                  <>
+                  <span key={title}>
                     <span className="catalog-navigation__separator">
                       <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd" clipRule="evenodd" d="M5.52876 3.52861C5.78911 3.26826 6.21122 3.26826 6.47157 3.52861L10.4716 7.52861C10.7319 7.78896 10.7319 8.21107 10.4716 8.47141L6.47157 12.4714C6.21122 12.7318 5.78911 12.7318 5.52876 12.4714C5.26841 12.2111 5.26841 11.789 5.52876 11.5286L9.05735 8.00001L5.52876 4.47141C5.26841 4.21107 5.26841 3.78896 5.52876 3.52861Z" fill="#B4BDC4" />
@@ -97,7 +103,7 @@ export const Catalog = ({ productName, phones, pathName }: CatalogProps) => {
                     >
                       {title}
                     </a>
-                  </>
+                  </span>
                 );
               })}
             </div>
